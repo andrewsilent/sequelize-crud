480a32e5b700f99230ee6d0225033100aa340d4b
andrewsilent <silent.netix@gmail.com>
2021-04-04 19:45:00 +0300

    add user controller

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -4,7 +4,6 @@ module.exports.createUser = async (req, res, next) => {
   try {
     const { body } = req;
     const createdUser = await User.create(body);
-    console.log(createdUser);
     res.status(201).send({ data: createdUser });
   } catch (err) {
     next(err);
@@ -15,10 +14,62 @@ module.exports.getAllUsers = async (req, res, next) => {
   try {
     const users = await User.findAll({
       attributes: {
-        exclude: ['password']
-      }
+        exclude: ['password'],
+      },
     });
     res.status(200).send({ data: users });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.getUser = async (req, res, next) => {
+  try {
+    const { userInstance } = req;
+    userInstance.password = undefined;
+    res.status(200).send({ data: userInstance });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.updateStaticUser = async (req, res, next) => {
+  try {
+    const {
+      body,
+      params: { id },
+    } = req;
+    const [rowsCount, [updatedUser]] = await User.update(body, {
+      where: { id },
+      returning: true,
+    });
+    updatedUser.password = undefined;
+    res.status(202).send({ data: updatedUser });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.updateUser = async (req, res, next) => {
+  try {
+    const { body, userInstance } = req;
+    updatedUserInstance = await userInstance.update(body, { returning: true });
+    updatedUser.password = undefined;
+    res.status(202).send({ data: updatedUserInstance });
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports.deleteUser = async (req, res, next) => {
+  try {
+    const {
+      params: { id },
+    } = req;
+    const userToDelete = await User.findByPk(id);
+    const result = await userToDelete.destroy();
+    updatedUser.password = undefined;
+    res.status(202).send({ data: userToDelete });
   } catch (err) {
     next(err);
   }
--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const UserController = require('./controller/user.controller');
 const TaskController = require('./controller/task.controller');
+const { checkUser } = require('./middlewares/user.mw');
 const router = Router();
 
 // router.use('/', (req, res, next) => {
@@ -25,6 +26,10 @@ router.get('/test', (req, res, next) => {
 router.post('/user', UserController.createUser);
 
 router.get('/users', UserController.getAllUsers);
+router.get('/user/:id', checkUser, UserController.getUser);
+router.patch('/user/:id', checkUser, UserController.updateUser);
+router.patch('/user-static/:id', UserController.updateStaticUser);
+router.delete('/user/:id', UserController.deleteUser);
 
 router.post('/user/:id/task', TaskController.createTask);
 
